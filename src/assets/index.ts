774a6e4dbb29489425885c031e7a97009a9eaee1
Sinh Nguyen <sinhnguyen.dev@gmail.com>
2024-05-09 19:30:39 +0700

    feat: update icon restaurant

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,6 @@
 export * from "./icon-adult";
 export * from "./icon-children";
+export * from "./icon-address";
+export * from "./icon-price";
+export * from "./icon-time";
+export * from "./icon-type";
