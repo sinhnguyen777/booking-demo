ee0e631455b9510064fa6bbf07a8feaa5d8528f4
Sinh Nguyen <sinhnguyen.dev@gmail.com>
2024-05-09 19:58:50 +0700

    fix: error route dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
-
-export default Dashboard;
+export default function Page() {
+  return <h1>Hello, Dashboard Page!</h1>;
+}
